f6af4588e6d37ffea85b8fdaba1cce34e979093f
Daniel Marin <danielarturomt@gmail.com>
2022-07-19 20:24:57 +0100

    feat: session #2 changes

--- tests/metaplex-test.ts
+++ tests/metaplex-test.ts
@@ -1,3 +1,9 @@
+import {
+  bundlrStorage,
+  isMintWithMetadata,
+  Metaplex,
+  walletAdapterIdentity,
+} from "@metaplex-foundation/js";
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { getMint } from "@solana/spl-token";
@@ -6,40 +12,81 @@ import { Disco } from "../target/types/disco";
 
 describe("disco", () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.Disco as Program<Disco>;
   const eventBaseKeypair = anchor.web3.Keypair.generate();
+  const metadataProgramPublicKey = new anchor.web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+  const metaplex = Metaplex.make(provider.connection)
+    .use(walletAdapterIdentity(provider.wallet))
+    .use(bundlrStorage());
 
   let eventPublicKey: anchor.web3.PublicKey;
   let ticketMintPublicKey: anchor.web3.PublicKey;
+  let metadataPublicKey: anchor.web3.PublicKey;
 
   before(async () => {
-    [eventPublicKey] = await anchor.web3.PublicKey.findProgramAddress([
-      Buffer.from('event', 'utf-8'),
-      eventBaseKeypair.publicKey.toBuffer()
-    ], program.programId);
-    [ticketMintPublicKey] = await anchor.web3.PublicKey.findProgramAddress([
-      Buffer.from('ticket_mint', 'utf-8'),
-      eventPublicKey.toBuffer()
-    ], program.programId)
-  })
+    [eventPublicKey] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("event", "utf-8"), eventBaseKeypair.publicKey.toBuffer()],
+      program.programId
+    );
+    [ticketMintPublicKey] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("ticket_mint", "utf-8"), eventPublicKey.toBuffer()],
+      program.programId
+    );
+    [metadataPublicKey] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata", "utf-8"),
+        metadataProgramPublicKey.toBuffer(),
+        ticketMintPublicKey.toBuffer(),
+      ],
+      metadataProgramPublicKey
+    );
+  });
 
-
-  it("Is initialized!", async () => {
+  it("should create an event", async () => {
+    // arrange
+    const ticketTitle = "Tomorrowland 2022";
+    const ticketSymbol = "TMRLND2022";
+    const ticketURI = "https://www.gooogle.com";
     // act
-    await program.methods.createEvent().accounts({
-      authority: provider.wallet.publicKey,
-      eventBase: eventBaseKeypair.publicKey,
-    }).rpc();
+    await program.methods
+      .createEvent(ticketTitle, ticketSymbol, ticketURI)
+      .accounts({
+        authority: provider.wallet.publicKey,
+        eventBase: eventBaseKeypair.publicKey,
+        metadataProgram: metadataProgramPublicKey,
+        ticketMetadata: metadataPublicKey,
+      })
+      .rpc();
     // assert
     const eventAccount = await program.account.event.fetch(eventPublicKey);
-    const ticketMintAccount = await getMint(provider.connection, ticketMintPublicKey);
-    
-    console.log(eventAccount);
-    console.log(ticketMintAccount);
-
+    const ticketMintAccount = await getMint(
+      provider.connection,
+      ticketMintPublicKey
+    );
+    const metaplexNft = await metaplex
+      .nfts()
+      .findMintWithMetadataByAddress(ticketMintPublicKey)
+      .run();
     assert.isDefined(eventAccount);
+    assert.equal(eventAccount.ticketTitle, ticketTitle);
+    assert.equal(eventAccount.ticketSymbol, ticketSymbol);
+    assert.equal(eventAccount.ticketUri, ticketURI);
+    assert.isDefined(ticketMintAccount);
+    assert.equal(ticketMintAccount.decimals, 0);
+    assert.equal(ticketMintAccount.supply, BigInt(0));
+    assert.isDefined(metaplexNft);
+    assert.equal(metaplexNft.decimals, 0);
+    assert.isTrue(metaplexNft.supply.basisPoints.eq(new anchor.BN(0)));
+    assert.isTrue(isMintWithMetadata(metaplexNft)); 
+    if (isMintWithMetadata(metaplexNft)) {
+      assert.equal(metaplexNft.metadata.name, ticketTitle);
+      assert.equal(metaplexNft.metadata.symbol, ticketSymbol);
+      assert.equal(metaplexNft.metadata.uri, ticketURI);
+    }
   });
 });
